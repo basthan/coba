9cd03fe5c8fd3742f4b2824b3ef66c52565bb8b3
basthan <basthanihsan@student.upi.edu>
2019-08-03 09:19:37 +0700

    jadi php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Ada PHP</h1>
+<audio src="http://us4.internet-radio.com:8266/stream" controls autoplay></audio>
+<?php
+echo 'Hello World!';
+?>
+</body>
+</html>
